483ac82ad51b0d3a47ebeef9106229dda9afda4e
Thomas Countz <thomascountz@gmail.com>
2018-06-03 10:40:32 -0400

    remove .gem and add requires

--- lib/lolli.rb
+++ lib/lolli.rb
@@ -1,4 +1,8 @@
 require "lolli/version"
+require "lolli/example"
+require "lolli/resultant"
 
-module Lolli
-end
+require "lolli/reporters/reporter"
+require "lolli/reporters/text_reporter"
+
+require "lolli/matchers/equality_matchers"
